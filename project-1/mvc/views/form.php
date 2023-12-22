6221745e75ec0cad602279fc77c2a14922e0963d
Andres <134742998+andreshamburger@users.noreply.github.com>
2023-12-22 15:55:33 -0500

    Delete project-1/mvc directory

--- project-1/mvc/views/form.php
+++
@@ -1,7 +0,0 @@
-<form method="POST">
-    <input type="text" name="species_name" placeholder="Name of the Species" value="<?php echo (isset($_POST['species_name']) ? $_POST['species_name'] : ""); ?>">
-    <input type="text" name="species_price" placeholder="Cost of the species" value="<?php echo (isset($_POST['species_price']) ? $_POST['species_price'] : ""); ?>">
-    
-    <input type="submit" name="submit" value="Submit">
-    <input type="reset" name="reset" value="Reset">
-</form>
