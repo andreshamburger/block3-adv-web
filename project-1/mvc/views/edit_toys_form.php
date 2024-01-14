c7f69107533db686599e93750159eff0a0e49c31
Andres <134742998+andreshamburger@users.noreply.github.com>
2024-01-14 15:11:28 -0500

    Add files via upload

---
+++ project-1/mvc/views/edit_toys_form.php
@@ -0,0 +1,13 @@
+<h2>Edit Toys</h2>
+
+<form method="POST">
+    <input type="hidden" name="toys_id" value="<?php echo $toys['toys_id']; ?>">
+
+    <label for="toys_name">Name</label>
+    <input type="text" name="toys_name" placeholder="Toys Name" value="<?php echo $toys['toys_name']; ?>">    
+
+    <label for="toys_price">Price</label>
+    <input type="text" name="toys_price" placeholder="Toys Price" value="<?php echo $toys['toys_price']; ?>">
+
+    <button type="submit" name="update_toys" class="btn">Update</button>
+</form>
