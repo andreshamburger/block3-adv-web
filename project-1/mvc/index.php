ce2cefdfa027c040b46bbea642dfc212d67f2f0a
Andres <134742998+andreshamburger@users.noreply.github.com>
2024-01-15 12:33:44 -0500

    Add files via upload

---
+++ project-1/mvc/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>project 1 - Andres Hamburger</title>
+   <link rel="stylesheet" href="assets/style.css">
+</head>
+
+<body>
+   <h1 class="indextitle">Pet Sanctuary MVC</h1>
+  
+
+   <?php
+   include_once("controllers/controller.php");
+   ?>
+   
+</body>
+
+</html>
---
+++ project-1/mvc/views/controller_menu.php
@@ -0,0 +1,21 @@
+<section class="controller_menu">
+
+    <div class="controller_menu_flex">
+        
+    <div class="controller_content">            
+            <a href="?page=pets" class="btn one">Show Pets</a>
+            <a href="?page=add_new_pets" class="btn one">Add Pets</a>
+
+            <a href="?page=breeds" class="btn two">Show Breeds</a>
+            <a href="?page=add_new_breeds" class="btn two">Add Breeds</a>
+
+            <a href="?page=species" class="btn three">Show Species</a>
+            <a href="?page=add_new_species" class="btn three">Add Species</a>
+
+            <a href="?page=toys" class="btn four">Show Toys</a>
+            <a href="?page=add_new_toys" class="btn four">Add Toys</a>
+        </div>
+
+    </div>
+
+</section>
